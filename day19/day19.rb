e73aa3d427b1acd32fd64b4d05053a9275ceecbd
Brendan Boyle <brendan.boyle@shopify.com>
2023-12-19 19:08:09 -0500

    day19 2

--- day19/day19.rb
+++ day19/day19.rb
@@ -10,7 +10,7 @@ class Day19
   end
 
   def part_two
-    @system.build_map
+    @system.find_all
   end
 end
 
@@ -32,33 +32,148 @@ class MachineRatingSystem
 
   end
 
-  def build_map
-    count = 0
-    x = 0
-    m = 0
-    a = 0
-    s = 0
-    4000.times do
-      m = 0
-      a = 0
-      s = 0
-      x += 1
-      4000.times do
-        a = 0
-        s = 0
-        m += 1
-        4000.times do
-          s = 0
-          a += 1
-          4000.times do
-            s += 1
-            p "Count #{x} #{m} #{a} #{s}"
-          end
+  def find_all
+
+    @rating_map = {}
+
+    set_from_workflow 'in'
+
+    @rating_map = change_map
+
+    create_map_from_accepted
+
+    @map_from_accepted.keys.each do |key|
+      puts "\n\n"
+      p key
+      @map_from_accepted[key].each do |row|
+        puts "\n"
+        row.each do |item|
+          p item
         end
       end
     end
-    p 'done'
+
+    51
+
   end
+
+  def create_map_from_accepted
+
+    @map_from_accepted = {}
+
+    @rating_map["A"].keys.each do |key|
+      @rating_map["A"][key].each_with_index do |row, i|
+
+        @map_from_accepted[i] = {} unless @map_from_accepted[i]
+
+        work_backwards key, row, i
+
+      end
+    end
+  end
+
+  def work_backwards(letter, row, index)
+
+    return if row[0] == "in"
+
+    @map_from_accepted[index][letter] = []  unless @map_from_accepted[index][letter]
+
+    @map_from_accepted[index][letter].push row
+
+    @rating_map[row[0]].keys.each do |key|
+      @rating_map[row[0]][key].each do |row|
+        work_backwards(key, row, index)
+      end
+    end
+
+
+  end
+
+  def change_map
+    new_map = {}
+    @rating_map.keys.each do |key|
+
+      new_map[key] = {}
+
+      @rating_map[key].each do |row|
+        row.each do |item|
+
+          new_map[key][item[1]] = [] unless  new_map[key][item[1]]
+
+          new_map[key][item[1]].push [item[0], item[2]]
+
+        end
+      end
+
+    end
+    new_map
+  end
+
+  def set_from_workflow(input)
+
+    unless input == "R" || input == "A"
+    # p "input #{input}"
+    end
+
+    workflow = @workflows[input]
+
+    return unless workflow
+
+    rest = []
+    next_list = []
+
+    workflow.rules.each do |rule|
+      next_list.push rule["destination"]
+
+      if rule["conditional"]
+        current = build_checker_array(input, rule["conditional"])
+        rest.push build_checker_array(input, rule["conditional"], true)
+        @rating_map[rule["destination"]] = [] unless @rating_map[rule["destination"]]
+        @rating_map[rule["destination"]].push [current]
+      else
+        @rating_map[rule["destination"]] = [] unless @rating_map[rule["destination"]]
+        @rating_map[rule["destination"]].push rest
+      end
+    end
+
+    next_list.each do |destination|
+      set_from_workflow destination
+    end
+
+  end
+
+
+
+  def build_checker_array(input, conditional, opposite = false)
+
+    item = nil
+    conditional_array = conditional.scan(/(\w)(>|<)(\d*)/)[0]
+
+    unless opposite
+
+      if conditional_array[1] == "<"
+        item = [input, conditional_array[0], [1, conditional_array[2].to_i - 1]]
+      end
+
+      if conditional_array[1] == ">"
+        item = [input, conditional_array[0], [conditional_array[2].to_i + 1, 4000]]
+      end
+
+    else
+
+      if conditional_array[1] == "<"
+        item = [input, conditional_array[0], [conditional_array[2].to_i, 4000]]
+      end
+
+      if conditional_array[1] == ">"
+        item = [input, conditional_array[0], [1, conditional_array[2].to_i]]
+      end
+
+    end
+
+    item
+  end
+
 
   def find_ratings
 
